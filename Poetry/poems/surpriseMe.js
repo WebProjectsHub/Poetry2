9281c573f07c4abfe0c9aa44f21971b6d147f760
WebProjectsHub <143710009+WebProjectsHub@users.noreply.github.com>
2023-09-16 01:33:52 +0400

    Add files via upload

--- Poetry/poems/surpriseMe.js
+++ Poetry/poems/surpriseMe.js
@@ -5,6 +5,7 @@ var urls3 =
 	"../poems/An Empty Meadow.html",
 	"../poems/Desiderata.html",
 	"../poems/Do Not Go Gentle Into That Good Night.html",
+	"../poems/Fire and Ice .html"
 	"../poems/If.html",
 	"../poems/Immortality.html",
 	"../poems/Into the Fray.html", 
@@ -13,7 +14,8 @@ var urls3 =
 	"../poems/Sonnet 18 Shall I compare thee to a summer’s day.html",
 	"../poems/The Journey.html",
 	"../poems/The Night is Darkening Round Me.html",
-	"../poems/The Visitor.html"
+	"../poems/The Visitor.html",
+	"../poems/Untitled.html"
 	];
 
 // A function to get a random URL from the array
